3e3a04950ddd7c4fc6991dc51479d1298490334b
Marius <mariusarzoiu05@gmail.com>
2026-02-23 14:26:26 +0200

    Make PWA installable (icons + service worker)

--- sw.js
+++ sw.js
@@ -1,3 +1,29 @@
-self.addEventListener("install", event => {
-  console.log("Service Worker installed");
+const CACHE_NAME = "plantguard-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.png",
+  "./icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
 });
